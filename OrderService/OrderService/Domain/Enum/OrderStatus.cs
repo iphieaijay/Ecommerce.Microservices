b515c5caa158524203fbd41c9416cd0bc2f57edd
iphieaijay <iphieaijay@outlook.com>
2026-01-23 23:43:42 +0100

    Project setup

---
+++ OrderService/OrderService/Domain/Enum/OrderStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderService.Domain.Enum
+{
+    public enum OrderStatus
+    {
+        Pending = 0,
+        Reserved = 1,
+        Confirmed = 2,
+        Shipped = 3,
+        Delivered = 4,
+        Cancelled = 5,
+        Failed = 6
+    }
+}
